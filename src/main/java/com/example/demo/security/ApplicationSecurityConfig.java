dbc8253d7fb9339d7df821f9dc4d094aab3e6969
ollleg111 <oleg.maliy14@gmail.com>
2021-03-17 00:54:01 +0200

    add new classes, change logic

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.demo.security;
 
+import com.example.demo.auth.ApplicationUserService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,20 +18,30 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.concurrent.TimeUnit;
 
 import static com.example.demo.security.ApplicationUserRole.*;
 import static com.example.demo.security.ApplicationUserPermission.*;
 
+//  git checkout 6-Form_Based_Authentication
+
 @Configuration
 @EnableWebSecurity
-@AllArgsConstructor
-
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 // TODO эта аннотация добавляется для переноса всех пермишенов из .antMatchers этого класса в аннотации
 // TODO @PreAuthorize класса StudentManagementController
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
+        this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
+    }
 
     // первый вариант
 
@@ -64,7 +78,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
                 // TODO 4) изменения в авторизации - меняем .name() на .getPermission()
                 // TODO 5) убераем описание от сюда и переносим его в аннотации в StudentManagementController
-
 //                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
 //                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
@@ -79,9 +92,41 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
-    }
 
+                // TODO 6) включение BASE_AUTIFICATION
+//                .httpBasic();
+
+                // TODO кастомизация странички login
+                // TODO добавляем в pom - spring-boot-starter-thymeleaf
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                //TODO пишем перенаправление - редирект на страницу courses в случае верного введения логина и пароля
+                    .defaultSuccessUrl("/courses",true)
+                //TODO доп параметры, можно и без них, которые должны совпадать с html
+                    .passwordParameter("password")
+                    .usernameParameter("username")
+                .and()
+                .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured")
+                    .rememberMeParameter("remember-me")
+                //TODO доп параметры, можно и без них, которые должны совпадать с html
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+                // TODO 8) если убрать .csrf().disable() - то эту строку тоже нужно убрать
+                // TODO выставляем http метод для logout - GET, но в случае .csrf().disable() можем выставить любой
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
+                // TODO 7) прописываем действие если выходим по logout - удаление из кукис JSESSIONID и значения  remember-me тк
+                // TODO они сохнаянются на 30 мин
+    }
+    // TODO улучшение кода - появился класс FakeApplicationDaoService и ApplicationUserDao
+/*
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
@@ -114,5 +159,19 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 lindaUser,
                 tomUser
         );
+    }
+ */
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
     }
 }
